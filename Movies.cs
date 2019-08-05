78d4b033eb12b971a2d523bea815628331dff0ee
mitch z <outvisible@gmail.com>
2019-08-05 13:29:42 -0400

    added a tab and a console.clear

--- Movies.cs
+++ Movies.cs
@@ -65,7 +65,78 @@ namespace MidtermNew
                 Console.WriteLine("That is not a valid title.");
                 return FilterMoviesByTitle(movieList);
             }
-        }
+            while (go)
+            {
+                Console.WriteLine("What is the movies's title?");
+                string input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    newMovie.Title = input;
+                    go = false;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid title.\n");
+                    go = true;
+                }
+            }
+            go = true;
+            while (go)
+            {
+                Console.WriteLine("Who is the director?");
+                string input = Console.ReadLine();
+                if (Regex.IsMatch(input, @"^[a-zA-Z .']+$"))
+                {
+                    newMovie.Director = input;
+                    go = false;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid author.\n");
+                    go = true;
+                }
+            }
+            go = true;
+            while (go)
+            {
+                Console.WriteLine("What genre is this movie?");
+                string input = Console.ReadLine();
+                if (Regex.IsMatch(input, @"^[a-zA-Z /-]+$"))
+                {
+                    newMovie.Genre = input;
+                    go = false;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid year.\n");
+                    go = true;
+                }
+            }
+            go = true;
+            Console.WriteLine("What year was it published?");
+            string year = Console.ReadLine();
+            while (go)
+            {
+                if (Regex.IsMatch(year, @"^[0-9]{4}$"))
+                {
+                    newMovie.Year = year;
+                    go = false;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid year.\n");
+                    go = true;
+                }
+            }
+           
+            newMovie.CheckedOut = "On shelf";
+            newMovie.DueDate = "Not checked out";
+            string random = Program.RandomString(10);
+            newMovie.Barcode = $"MT{random}";
+            movies.Add(newMovie);
+            movies.OrderBy(x => x.Title).ToList();
+            Console.WriteLine($"{newMovie.Title} was successfully added.\n");
 
+        }
     }
 }
